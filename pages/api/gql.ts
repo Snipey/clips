e9f1941688580dd953ca3da403efc9fc61efba9d
Stephen F <snipeydev@gmail.com>
2020-04-21 03:20:50 -0700

    gql hotfix

--- pages/api/gql.ts
+++ pages/api/gql.ts
@@ -64,7 +64,7 @@ const updateCategories = async (): Promise<void> => {
     }
   ).then((data) => data.json());
 
-  categories = data.map((category: Category) => ({
+  categories = data.contentObjects.map((category: Category) => ({
     ...category,
   }));
   lastUpdated = new Date();
